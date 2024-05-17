d0a57be5070bf1032b5301e3405f3acdda690100
wiKanok <kanokwan1401.ks@gmail.com>
2024-05-17 14:25:24 +0700

    feat:callbacks ex1-2

--- ex-2.js
+++ ex-2.js
@@ -2,7 +2,25 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let passingScore = 0;
+  for (let i = 0; i < array.length; i++) {
+    passingScore += operation(array[i]);
+  }
+  // console.log(passingScore);
+  if (passingScore >= 5) {
+    console.log("ผ่านเกณฑ์ ✅");
+  } else {
+    console.log("ไม่ผ่านเกณฑ์ ❌");
+  }
 }
+
+const result = function (score) {
+  if (score >= 70) {
+    return 1;
+  } else return 0;
+  // console.log(passingScore);
+};
+// console.log(passingScore);
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
@@ -10,6 +28,6 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, result);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, result);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, result);
